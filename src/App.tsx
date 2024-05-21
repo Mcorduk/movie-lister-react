81f042785a8adf4269ff3256bbdf36c34a04016e
Mcorduk <om.corduk@gmail.com>
2024-05-21 18:09:23 +0300

    feat: Add global css for reset, wrapper around the movie list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // App.tsx
-import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Layout from "./components/layout/Layout";
-import MovieItem from "./components/MovieItem";
+import MovieList from "./components/MovieList";
+import "./styles/global.scss";
+import { Container } from "@mui/material";
 
 const theme = createTheme({
   breakpoints: {
@@ -19,7 +20,13 @@ const theme = createTheme({
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>{<MovieItem />}</Layout>
+      <Layout>
+        {
+          <Container className="wrapper">
+            <MovieList />
+          </Container>
+        }
+      </Layout>
     </ThemeProvider>
   );
 };
