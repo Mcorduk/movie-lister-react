b4244f65a3ec42653aec05cde3852d47fad9f2b9
Mcorduk <om.corduk@gmail.com>
2024-05-21 11:23:42 +0300

    feat: Update App component with ThemeProvider and a placeholder MovieItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.css";
+// App.tsx
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Layout from "./components/layout/Layout";
+import MovieItem from "./components/MovieItem";
 
-function App() {
-  return <></>;
-}
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>{<MovieItem />}</Layout>
+    </ThemeProvider>
+  );
+};
 
 export default App;
