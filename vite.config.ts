c4caf6f5c33d10f9a9916501b8f6640a376f75bf
Mcorduk <om.corduk@gmail.com>
2024-05-22 14:52:26 +0300

    chore: Update vite.config.ts to set base path for movie-lister-react

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "movie-lister-react",
+  base: "/movie-lister-react/",
   plugins: [react()],
 });
