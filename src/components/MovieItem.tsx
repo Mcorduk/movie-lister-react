d0843ee02dec791569a97e7b31208b5dcaa22928
Mcorduk <om.corduk@gmail.com>
2024-05-22 11:34:56 +0300

    refactor: Add react-router-dom Link to MovieItem component for viewing movie details

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -2,15 +2,10 @@
 import React from "react";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import styles from "./MovieItem.module.scss";
+import { Link } from "react-router-dom";
+import { Movie } from "../store/movie/movieSlice";
 
-interface MovieItemProps {
-  movie: {
-    Title: string;
-    Year: string;
-    Poster: string;
-  };
-}
-const MovieItem: React.FC<MovieItemProps> = ({ movie }) => {
+const MovieItem: React.FC<{ movie: Movie }> = ({ movie }) => {
   return (
     <Card className={styles.movieItem}>
       <CardMedia
@@ -26,6 +21,9 @@ const MovieItem: React.FC<MovieItemProps> = ({ movie }) => {
         <Typography variant="body2" color="text.secondary">
           {movie.Year}
         </Typography>
+        <Link to={`/movie/${movie.imdbID}`} className={styles.viewButton}>
+          View Details
+        </Link>
       </CardContent>
     </Card>
   );
